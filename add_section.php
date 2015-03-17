ee2a9f49af8138f72badb967cf9d05cde1a72901
Yuze Lei <yuze.lei@valpo.edu>
2015-03-17 15:48:43 +0000

    add add_section.php

---
+++ add_section.php
@@ -0,0 +1,23 @@
+<!--This is the html page that displays a form for adding a section -->
+<!DOCTYPE html>
+<?php
+
+$page_title = 'ADD SECTION';
+include ('index.html');
+?>
+ <html>
+  <div id='section'>
+  <head>
+   <meta charset="utf-8">
+   <title></title>
+  </head>
+
+ 
+  <body>
+   <form action = "" method = "POST">
+
+   </form>
+  </body>
+
+</div>
+</html>
