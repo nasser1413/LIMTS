b0fa4ea74e8aa29a5ad49583e707fab1c1a01edd
Nasser Almawash <nasser.almawash@valpo.edu>
2015-04-09 14:19:24 +0000

    checking entries

--- add_room.php
+++ add_room.php
@@ -1,15 +1,64 @@
 <script type="text/javascript">
-loadSelector("building", function() {
+
+function onFormSubmitted() {
+
+	var building = $("#building-selector option:selected").val();
+	var number = $("#room-number").val();
+	var capacity = $("#maxCapacity1").val();
+	var handicap_accessible = $("#").val();
+
+	if (!building ||!number || !capacity) {
+        if (!building) {
+            $("#building-selector").parents(".form-group").addClass("has-error");
+        }
+	 if (!number) {
+            $("#room-number").parents(".form-group").addClass("has-error");
+        }
+     	if (!capacity) {
+            $("#maxCapacity1").parents(".form-group").addClass("has-error");
+        }  
+
+  $("#warning-alert").offcanvas("show");
+
+}
+}
+$(function() {
+
+ 	$("#building-selector option:selected").change(function() {
+        var parent = $("#building-selector").parents(".form-group");
+        parent.removeClass("has-error");
+        parent.addClass("has-success");
+    });
+
+  	$("#room-number").change(function() {
+        var parent = $("#room-number").parents(".form-group");
+        parent.removeClass("has-error");
+        parent.addClass("has-success");
+    });
+ 	$("#maxCapacity1").change(function() {
+        var parent = $("#maxCapacity1").parents(".form-group");
+        parent.removeClass("has-error");
+        parent.addClass("has-success");
+    });
+
+$('div.btn-group .btn').click(function(){
+  $(this).find('input:radio').attr('checked', true);
+  alert($('input[name=radio-btn-ctrl]:checked').val());
+});
+
+	loadSelector("building", function() {
     alert("you selected something...");
 }, {
     addBlank: true,
     multiselect: false
 });
+
+});
 </script>
 
 
-<div class="alert alert-danger alert-fixed-top offcanvas" id="warningAlert1">
-  <strong>Success!</strong> Your action has been completed succefully.
+<div class="alert alert-danger alert-fixed-top offcanvas" id="warning-alert">
+  <strong>Error!</strong> You must fill out the highlighted fields!
 </div>
 
 <h1>Add Room</h1>
@@ -33,9 +82,9 @@ loadSelector("building", function() {
 
     <div class="form-group">
     <label for="handicapAccessible1">Handicap Accessible:</label>
-    <label class="radio-inline"><input type="radio" name="optradio">Yes</label>
-    <label class="radio-inline"><input type="radio" name="optradio">No</label>
-	</div>
+    <label class="radio-inline"><input type="radio" name="optradio" >Yes</label>
+    <label class="radio-inline"><input type="radio" name="optradio" >No</label>	
+</div>
 
     <div class="form-group">
     <input type="submit" class="btn btn-default" value="Submit">
