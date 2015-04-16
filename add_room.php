4aff8be06897475c923bc95060309f20686acf8d
Justin Szaday <wingpad@gmail.com>
2015-04-16 14:49:33 +0000

    Fixed Rooms code...

--- add_room.php
+++ add_room.php
@@ -1,28 +1,47 @@
 <script type="text/javascript">
 
 function onFormSubmitted() {
-
 	var building = $("#building-selector option:selected").val();
 	var number = $("#room-number").val();
-	var capacity = $("#maxCapacity1").val();
-	var handicap_accessible = $("#").val();
+	var capacity = $("#max-capacity").val();
+	var handicap_accessible = $("#handicap-accessible").is(':checked');
 
-	if (!building ||!number || !capacity) {
+	if (!building || !number || !capacity) {
         if (!building) {
             $("#building-selector").parents(".form-group").addClass("has-error");
         }
-        if (!number) {
+
+		if (!number) {
             $("#room-number").parents(".form-group").addClass("has-error");
         }
+
      	if (!capacity) {
-            $("#maxCapacity1").parents(".form-group").addClass("has-error");
-        }  
+            $("#max-capacity").parents(".form-group").addClass("has-error");
+        }
 
         $("#warning-alert").offcanvas("show");
-    }
+    } else {
+		$.ajax({
+			dataType: "json",
+			url: "create_room.php",
+			data: {
+				handicapAccessible: handicap_accessible,
+				capacity: capacity,
+				number: number,
+				building: building
+			},
+			success: function(data) {
+				if (data.response !== "Success") {
+					alert(data.response);
+				} else {
+					window.location.href = "?page=calendar" + location.hash;
+				}
+			}
+		});
+	}
 }
-$(function() {
 
+$(function() {
  	$("#building-selector option:selected").change(function() {
         var parent = $("#building-selector").parents(".form-group");
         parent.removeClass("has-error");
@@ -34,59 +53,56 @@ $(function() {
         parent.removeClass("has-error");
         parent.addClass("has-success");
     });
- 	$("#maxCapacity1").change(function() {
-        var parent = $("#maxCapacity1").parents(".form-group");
+
+ 	$("#max-capacity").change(function() {
+        var parent = $("#max-capacity").parents(".form-group");
         parent.removeClass("has-error");
         parent.addClass("has-success");
     });
 
-$('div.btn-group .btn').click(function(){
-  $(this).find('input:radio').attr('checked', true);
-  alert($('input[name=radio-btn-ctrl]:checked').val());
-});
-
 	loadSelector("building", function() {
-    alert("you selected something...");
-}, {
-    addBlank: true,
-    multiselect: false
-});
-
+		var parent = $("#building-selector").parents(".form-group");
+		parent.removeClass("has-error");
+		parent.addClass("has-success");
+	}, {
+	    addBlank: true,
+	    multiselect: false
+	});
 });
 </script>
 
 
 <div class="alert alert-danger alert-fixed-top offcanvas" id="warning-alert">
-  <strong>Error!</strong> You must fill out the highlighted fields!
+	<strong>Error!</strong> You must fill out the highlighted fields!
 </div>
 
 <h1>Add Room</h1>
 <form action="javascript:onFormSubmitted()" id="mainForm">
 
     <div class="form-group">
-    <label for="building-selector">Building:</label>
-    <select class="form-control" id="building-selector" name="building">
-    </select>
+	    <label for="building-selector">Building:</label>
+	    	<select class="form-control" id="building-selector" name="building">
+	    </select>
     </div>
 
     <div class="form-group">
-    <label for="room-number">Room Number:</label>
-    <input type="text" class="form-control" id="room-number" name="room-number">
+	    <label for="room-number">Room Number:</label>
+	    <input type="text" class="form-control" id="room-number" name="room-number">
 	</div>
 
     <div class="form-group">
-    <label for="maxCapacity1">Room Capacity:</label>
-    <input type="text" class="form-control" id="maxCapacity1" name="maxCapacity">
+	    <label for="max-capacity">Room Capacity:</label>
+	    <input type="text" class="form-control" id="max-capacity" name="max-capacity">
     </div>
 
     <div class="form-group">
-    <label for="handicapAccessible1">Handicap Accessible:</label>
-    <label class="radio-inline"><input type="radio" name="optradio" >Yes</label>
-    <label class="radio-inline"><input type="radio" name="optradio" >No</label>	
-</div>
+		<label class="checkbox-inline">
+  			<input type="checkbox" id="handicap-accessible" value="handicap-accessible"> Handicap Accessible
+		</label>
+	</div>
 
     <div class="form-group">
-    <input type="submit" class="btn btn-default" value="Submit">
+    	<input type="submit" class="btn btn-default" value="Submit">
     </div>
 
 </form>
--- create_room.php
+++ create_room.php
@@ -1,46 +1,44 @@
 <?php
 	// Import the "Grab Bag"
 	require("common.php");
-	
+
 	// Open an (OO) MySQL Connection
 	$conn = new mysqli($GLOBALS["dbhost"], $GLOBALS["dbuser"], $GLOBALS["dbpass"], $GLOBALS["dbname"]);
-	
+
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
 
 	// Get all of the "Parameters"
-	$building = $_GET["Building"];
-	$number = $_GET["Number"];
-	$capacity = $_GET["Capacity"];
-	$handicap_accessible = $_GET["HandicapAccessible"];
+	$building = $_GET["building"];
+	$number = $_GET["number"];
+	$capacity = $_GET["capacity"];
+	$handicap_accessible = json_decode($_GET["handicapAccessible"]);
 
 	// Check to make sure the required information is present
 	if (!($building && $number && $capacity)) {
 		die("{\"response\": \"You must specify the building, number and capacity!\"}");
 	}
-	// Assume a default value for HandicapAccessible if it is null
-	if (!$handicap_accessible) {
-		$type = "1";
-	}
+
 	// Check to see if the Room already exists in the database
 	$result = $conn->query("SELECT *
-				FROM `Room`
-				WHERE `Building`='$building' AND 'Number'='$number'");
+							FROM `Room`
+							WHERE `Building`='$building' AND `Number`='$number'");
 	if ($result->num_rows > 0) {
 		die("{\"response\": \"Room already exists in database\"}");
 	}
 	$result->close();
+
 	// Everything seems ok at this point, so just add the room
-	$result = $conn->query("INSERT INTO `Room` (Building, Number, Capacity, HandicapAccessible)
-				VALUES('$building', '$number', '$capacity', '$handicap_accessible')");
+	$result = $conn->query("INSERT INTO `Room`(`Building`, `Number`, `Capacity`, `HandicapAccessible`)
+							VALUES('$building', '$number', '$capacity', '$handicap_accessible')");
 	if (!$result) {
 		die("{\"response\": \"Could not insert Room!\"}");
 	}
 
-	 // Give a success response
-    	echo "{\"response\": \"Success\"}";
+	// Give a success response
+	echo "{\"response\": \"Success\"}";
 
 	// Finally, close the connection
 	$conn->close();
