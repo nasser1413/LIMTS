8963daf49665476106f02651e86ee42a01e5c33c
Nasser Almawash <nasser.almawash@valpo.edu>
2015-04-23 13:33:20 +0000

    remove from DB

---
+++ remove_class.php
@@ -0,0 +1,50 @@
+<?php
+    // Import the "Grab Bag"
+    require("common.php");
+
+    // Open an (OO) MySQL Connection
+    $conn = new mysqli($GLOBALS["dbhost"], $GLOBALS["dbuser"], $GLOBALS["dbpass"], $GLOBALS["dbname"]);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Get all of the "Parameters"
+    $id = $_GET["id"];
+
+// Check to make sure the required information is present
+    if (!($id)) {
+      die("{\"response\": \"fail to get id!\"}");
+    }
+
+
+  //   Check to see if the Building  not exists in the database
+    $result = $conn->query("SELECT *
+                            FROM `Class`
+                            WHERE `id`='$id'");
+    if ($result->num_rows > 0) {
+ 		$result->close();
+
+	// ready to delete 
+		$result = $conn->query("DELETE FROM `Class`
+                            WHERE `id`='$id'");
+
+	  if (!$result) {
+      die("{\"response\": \"Could not remove the Class!\"}");
+    }
+    
+    }
+	else{
+ die("{\"response\": \"Class does not exists in database\"}");
+}
+   	
+
+
+  // Give a success response
+    echo "{\"response\": \"Success\"}";
+
+ // Finally, close the connection
+    $conn->close();
+
+?>
