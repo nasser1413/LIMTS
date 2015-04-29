9fd050b15c475f44dc9d198938638b75e64d6a9f
Justin Szaday <wingpad@gmail.com>
2015-04-29 21:48:50 +0000

    Added some documentation (Chinny)

--- about.php
+++ about.php
@@ -1,2 +1,33 @@
-<h1>About</h1>
-<p><i>LIMTS</i> is more than a scheduler!</p>
+<div class="row" style="padding: 0 20px 0">
+    <div class="page-header">
+    <h1>User Manual <small>for LIMTS</small></h1>
+    </div>
+
+    <p class="lead">
+        Thank you for choosing to use LIMTS! This is a simple guide to allow you to start making use of the software immediately.<br>
+    </p>
+
+    <h3>Intro</h3>
+    <p>LIMTS can be used for making class schedules, managing a list of professors, classes and buildings, and keeping track of changes in important information such as classroom availability and space restrictions, among other things.</p>
+    <p>To begin, you need to first insert new data into LIMTS. This data can then be manipulated and used to make many decisions. </p>
+
+    <h3>Adding Data</h3>
+    <p>Let's start by adding a professor. To add a professor, click on the menubar and select "Add." You should see a dropdown menu. Then select the "Professor" option.</p>
+
+    <p>Now that we have a professor added, go back to the Add menu and click on Class.</p>
+
+    <p>Now that a class has been added, go back to the Add menu once more and click on Section. We can now combine the Professor and the class you added to LIMTS to make a new section, taught by the aforementioned professor at the meeting time you specify in the classroom(s) specified.</p>
+
+    <h3>Managing Data</h3>
+    <p>Say you made a mistake when you were adding a professor that you didn't catch until <i>after</i> you had submitted the form. To make corrections, click on the main menu and go to <q>Manage.</q> When you click on the Manage option, you will be directed to a page that gives you a submenu with a list of categories (e.g. Section, Class, Professor, Semester etc.). Click on the category you wish to edit/remove. Make whichever changes you want and click submit. A message should be displayed letting you know that your modifications were successful.
+    </p>
+
+    <h3>Viewing Data</h3>
+    <p>We should now have at least one class, one section of a class, and one professor stored in LIMTS. As you make continue to add more information, you may want to see an aggregate of your data or the current schedule. LIMTS provides two ways of doing this:
+    <ul>
+    <li>The Table View</li>
+    <li>The Calendar View</li>
+    </ul>
+    Both views allow you to specify parameters so you can look at only information you are interested in, and the calendar view allows you to look at a schedule.
+    </p>
+</div>
--- remove_professor.php
+++ remove_professor.php
@@ -7,44 +7,41 @@
 
     // Check connection
     if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
     }
 
     // Get all of the "Parameters"
     $id = $_GET["id"];
 
-// Check to make sure the required information is present
+    // Check to make sure the required information is present
     if (!($id)) {
-      die("{\"response\": \"fail to get id!\"}");
+        die("{\"response\": \"fail to get id!\"}");
     }
 
 
-  //   Check to see if the Building  not exists in the database
+    // Check to see if the Building  not exists in the database
     $result = $conn->query("SELECT *
                             FROM `Professor`
                             WHERE `id`='$id'");
-    if ($result->num_rows > 0) {
- 		$result->close();
+    $professor_exists = $result->num_rows > 0;
+    // Close the Result
+    $result->close();
 
-	// ready to delete 
-		$result = $conn->query("DELETE FROM `Professor`
-                            WHERE `id`='$id'");
+    if ($professor_exists) {
+        // ready to delete
+        $result = $conn->query("DELETE FROM `Professor`
+                                WHERE `id`='$id'");
 
-	  if (!$result) {
-      die("{\"response\": \"Could not remove the Professor!\"}");
+        if (!$result) {
+            die("{\"response\": \"Could not remove the Professor!\"}");
+        }
+    } else {
+        die("{\"response\": \"Professor does not exists in database\"}");
     }
-    
-    }
-	else{
- die("{\"response\": \"Professor does not exists in database\"}");
-}
-   	
 
-
-  // Give a success response
+    // Give a success response
     echo "{\"response\": \"Success\"}";
 
- // Finally, close the connection
+    // Finally, close the connection
     $conn->close();
-
 ?>
